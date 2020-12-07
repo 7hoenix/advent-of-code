ad65e0227642c63e13499819b2ecd5f2f579cba2
7hoenix <jpholzmann@gmail.com>
2020-12-07 09:01:26 -0600

    Day 6 part 2.

--- lib/days/day_6.ex
+++ lib/days/day_6.ex
@@ -16,9 +16,15 @@ defmodule Prog do
   end
 
   def extract(group) do
-    thing = String.split(group, "\n", trim: true)
-    all_in_group = Enum.flat_map(thing, fn line -> String.split(line, "", trim: true) end)
-    MapSet.new(all_in_group)
+    all_in_group = String.split(group, "\n", trim: true)
+    # all_in_group = Enum.flat_map(thing, fn line -> String.split(line, "", trim: true) end)
+    [h | rem] = all_in_group
+    all_shared_in_group = Enum.reduce(rem, s_to_map_set(h), fn line, acc -> MapSet.intersection(s_to_map_set(line), acc) end)
+    MapSet.new(all_shared_in_group)
+  end
+
+  def s_to_map_set(line) do
+    MapSet.new(String.split(line, "", trim: true))
   end
 end
 
