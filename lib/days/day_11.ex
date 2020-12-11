49434f5a4ad1080ba15fb9be4d16c24f0753bf3d
7hoenix <jpholzmann@gmail.com>
2020-12-11 10:38:14 -0600

    Day 11 Part 2.

--- lib/days/day_11.ex
+++ lib/days/day_11.ex
@@ -27,19 +27,23 @@ defmodule Prog do
     as_map = convert_to_map(data)
              |> Enum.reduce(Map.new(), fn row, acc ->
                 Enum.reduce(row, acc, &put_stuff/2) end)
-    part_1 = find_part_1(as_map, row_size, column_size)
-    IO.inspect part_1
+
+    part_1 = find_occupied_seats_after_stabalizing(as_map, row_size, column_size, &occupied_adjacent_seats/3, 4)
+    IO.inspect part_1, label: "Part 1"
+
+    part_2 = find_occupied_seats_after_stabalizing(as_map, row_size, column_size, &occupied_adjacent_seats_line_of_sight/3, 5)
+    IO.inspect part_2, label: "Part 2"
   end
 
-  def find_part_1(as_map, row_size, column_size) do
-    updates = apply_rules(as_map, row_size, column_size)
+  def find_occupied_seats_after_stabalizing(as_map, row_size, column_size, finder, adjecent_count) do
+    updates = apply_rules(as_map, row_size, column_size, finder, adjecent_count)
               |> Enum.reject(&is_nil/1)
     updated_map = Enum.reduce(updates, as_map, fn {coords, entry}, acc -> Map.put(acc, coords, entry) end)
     if updated_map == as_map do
-      print_waiting_room(updated_map, row_size, column_size)
+      # print_waiting_room(updated_map, row_size, column_size)
       count_occupied_seats(updated_map, row_size, column_size)
     else
-      find_part_1(updated_map, row_size, column_size)
+      find_occupied_seats_after_stabalizing(updated_map, row_size, column_size, finder, adjecent_count)
     end
   end
 
@@ -57,23 +61,23 @@ defmodule Prog do
       end)
   end
 
-  def apply_rules(data, row_size, column_size) do
+  def apply_rules(data, row_size, column_size, finder, adjecent_count) do
     Range.new(0, row_size)
     |> Enum.flat_map(fn row_index ->
       Range.new(0, column_size)
-      |> Enum.map(fn column_index -> apply_rules_to_cell(row_index, column_index, data) end)
+      |> Enum.map(fn column_index -> apply_rules_to_cell(row_index, column_index, data, finder, adjecent_count) end)
     end)
   end
 
-  def apply_rules_to_cell(y, x, data) do
-    adjacent_seats = occupied_adjacent_seats(y, x, data)
+  def apply_rules_to_cell(y, x, data, finder, adjecent_count) do
+    adjacent_seats = finder.(y, x, data)
     case Map.get(data, {y,x}) do
       :empty ->
         if adjacent_seats == 0 do
           {{y, x}, :occupied}
         end
       :occupied ->
-        if adjacent_seats >= 4 do
+        if adjacent_seats >= adjecent_count do
           {{y, x}, :empty}
         end
       _ ->
@@ -96,6 +100,37 @@ defmodule Prog do
     ]
     |> Enum.map(&occupied/1)
     |> Enum.sum()
+  end
+
+  def occupied_adjacent_seats_line_of_sight(y, x, data) do
+    [
+      occupied_seat_line_of_sight(y, fn y -> (y + 1) end, x, fn x -> (x - 1) end, data),
+      occupied_seat_line_of_sight(y, fn y -> (y + 1) end, x, fn x -> x end, data),
+      occupied_seat_line_of_sight(y, fn y -> (y + 1) end, x, fn x -> (x + 1) end, data),
+
+      occupied_seat_line_of_sight(y, fn y -> y end, x, fn x -> (x - 1) end, data),
+      occupied_seat_line_of_sight(y, fn y -> y end, x, fn x -> (x + 1) end, data),
+
+      occupied_seat_line_of_sight(y, fn y -> (y - 1) end, x, fn x -> (x - 1) end, data),
+      occupied_seat_line_of_sight(y, fn y -> (y - 1) end, x, fn x -> x end, data),
+      occupied_seat_line_of_sight(y, fn y -> (y - 1) end, x, fn x -> (x + 1) end, data),
+    ]
+    |> Enum.sum()
+  end
+
+  def occupied_seat_line_of_sight(y, y_fn, x, x_fn, data) do
+    next_y = y_fn.(y)
+    next_x = x_fn.(x)
+    case Map.get(data, {next_y, next_x}) do
+      nil ->
+        0
+      :occupied ->
+        1
+      :empty ->
+        0
+      :floor ->
+        occupied_seat_line_of_sight(next_y, y_fn, next_x, x_fn, data)
+    end
   end
 
   def occupied(seat) do
@@ -145,7 +180,7 @@ defmodule Prog do
   end
 
   def count_occupied_seats(data, row_size, column_size) do
-    IO.puts Range.new(0, row_size)
+    Range.new(0, row_size)
     |> Enum.map(fn row_index ->
       Range.new(0, column_size)
       |> Enum.map(fn column_index -> occupied(Map.get(data, {row_index, column_index})) end)
